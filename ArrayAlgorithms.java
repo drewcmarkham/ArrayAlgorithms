d6294c5051d584f04edbb350d9e5a924a81db8eb
drewcmarkham <drew.markham@icloud.com>
2025-02-13 08:57:02 -0800

    ArrayList algorithms finished

--- ArrayAlgorithms.java
+++ ArrayAlgorithms.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class ArrayAlgorithms {
 
     public static void main(String[] args) {
@@ -90,13 +92,32 @@ public class ArrayAlgorithms {
                 newArray[j] = numbers[i];
                 j++;
                 }
+
+            int[] reversed = new int[numbers.length];
+            for (int i = 0; i < numbers.length; i++) {
+                reversed[numbers.length -1 - i] = numbers[i];
+            }
             for (int num : newArray) {
                 System.out.print(num + " ");
             }
             System.out.println("");
 
-            //shift the array over by a variable int (moving the last to the front)
-            System.out.println("Shift By One:");
+            //shift the array over by a variable int 
+            System.out.println("Shifted Array");
+            for (int shift = -numbers.length * 2; shift < numbers.length * 2; shift++) {
+                int[] shifted = new int[numbers.length];
+                for (int i = 0; i < numbers.length; i++) {
+                    int nearZeroShift = (shift % numbers.length + numbers.length);
+                    shifted[(nearZeroShift + i) % numbers.length] = numbers[i];
+                }
+                for (int shiftedNum : shifted){
+                    System.out.print(shiftedNum + " ");
+                }
+                System.out.println("");
+            }
+
+            // (moving the last to the front)
+            System.out.println( "\nLast To Front:");
             int[] shiftArray = new int[numbers.length];
             for (int k = 1; k < numbers.length; k++) {
                 shiftArray[k] = numbers[k-1];
@@ -105,5 +126,98 @@ public class ArrayAlgorithms {
             for (int num : shiftArray) {
                 System.out.print(num + " ");
             }
+            System.out.println("");
+
+            //ArrayList Algorithms
+
+            ArrayList<Integer> numbersList = new ArrayList<Integer>();
+            // add 10 random numbers
+            for (int i = 0; i < 10; i++) {
+                numbersList.add((int)(Math.random() * 20));
+            }
+
+            // print the list
+            System.out.println("Print List:");
+
+            System.out.println(numbersList);
+
+            // sum of the list
+            System.out.println("Sum:");
+
+            int total = 0;
+            for (int num : numbersList) {
+                total += num;
+            }
+            System.out.println(total);
+
+            // mean of the list
+            System.out.println("Mean:");
+
+            System.out.println(numbersList.size());
+
+            // smallest number in the list
+            System.out.println("Smallest Number:");
+            int z = numbersList.get(0);
+            for (int num : numbersList) {
+                if (num < z) {
+                    z = num;
+                }
+            }
+            System.out.println(z);
+
+            // biggest number in the list
+            System.out.println("Biggest Number:");
+            int v = numbersList.get(0);
+            for (int num : numbersList) {
+                if (num > v) {
+                    v = num;
+                }
+            }
+            System.out.println(v);
+
+            // mode of the list
+            System.out.println("Mode:");
+
+            int biggestIndex2 = 0;
+            int mostCommon2 = 0;
+            for (int num : numbersList) {
+                int commonCount2 = 0;
+                for (int num2 : numbersList) {
+                    if (num2 == num) {
+                        commonCount2++;
+                    }
+                }
+                if (commonCount2 > mostCommon2) {
+                    mostCommon2 =  commonCount2;
+                    biggestIndex2 = num;
+                }
+            }
+            System.out.println(biggestIndex2);
+
+            // reverse the list
+            System.out.println("Reversed Array List:");
+
+            ArrayList<Integer> reversedArrayList = new ArrayList<Integer>();
+            for (int i = 0; i < numbersList.size(); i++) {
+                reversedArrayList.add(0, numbersList.get(i));
+            }
+
+            System.out.println(reversedArrayList);
+
+            // shift the list over by a variable int
+            System.out.println("Shift Array List:");
+
+            for (int shift = -numbersList.size() * 2; shift < numbersList.size() * 2; shift++) {
+                ArrayList<Integer> shifted2 = new ArrayList<Integer>();
+                for (int i = 0; i < numbersList.size(); i++) {
+                    int nearZeroShift = (shift % numbersList.size() + numbersList.size());
+                    shifted2.add(i, numbersList.get((nearZeroShift + i) % numbersList.size()));
+                }
+                for (int shiftedNum : shifted2){
+                    System.out.print(shiftedNum + " ");
+                }
+                System.out.println("");
+            }
+
         }   
     }
